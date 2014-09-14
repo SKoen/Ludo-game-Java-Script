04d74f27f4111ecf1bbef0eb0736825832f3f45a
SKoen <slavi.koen@abv.bg>
2014-09-14 12:14:14 +0300

    Create 4 path for 4 paws

---
+++ test.js
@@ -0,0 +1,126 @@
+﻿var currPos =0;
+var step = 50;
+var currcolor = "";
+function stepDown() {
+    var doc = document.getElementById(currcolor + "paw");
+    var curr = Number(doc.style.top.replace(/[a-z]/g, ''));
+    doc.style.top = (curr+step)+'px';
+    currPos++;
+}
+function stepUp() {
+    var doc = document.getElementById(currcolor + "paw");
+    var curr = Number(doc.style.top.replace(/[a-z]/g, ''));
+    doc.style.top = (curr - step) + 'px';
+
+    currPos++;
+}
+function stepLeft() {
+    var doc = document.getElementById(currcolor + "paw");
+    var curr = Number(doc.style.left.replace(/[a-z]/g, ''));
+    doc.style.left = (curr - step) + 'px';
+    currPos++;
+}
+function stepRight() {
+    var doc = document.getElementById(currcolor+"paw");
+    var curr = Number(doc.style.left.replace(/[a-z]/g, ''));
+    doc.style.left = (curr + step) + 'px';
+    currPos++;
+}
+var stepsRed = [];
+var stepsYellow = [];
+var stepsBlue =[];
+var stepsGreen =[];
+function pushSteps(value, steps, count) {
+    for (i = 0; i < count; i++) steps.push(value);
+}
+//Red pawn
+pushSteps(stepDown,stepsRed,4);
+pushSteps(stepRight, stepsRed,4);
+pushSteps(stepDown, stepsRed,2);
+pushSteps(stepLeft, stepsRed,4);
+pushSteps(stepDown, stepsRed,4);
+pushSteps(stepLeft, stepsRed,2);
+pushSteps(stepUp, stepsRed,4);
+pushSteps(stepLeft, stepsRed,4);
+pushSteps(stepUp, stepsRed,2);
+pushSteps(stepRight, stepsRed,4);
+pushSteps(stepUp, stepsRed,4);
+pushSteps(stepRight, stepsRed,1);
+pushSteps(stepDown, stepsRed,5);
+//Yellow token
+
+pushSteps(stepUp, stepsYellow,4);
+pushSteps(stepLeft, stepsYellow,4);
+pushSteps(stepUp, stepsYellow,2);
+pushSteps(stepRight, stepsYellow,4);
+pushSteps(stepUp, stepsYellow,4);
+pushSteps(stepRight, stepsYellow,2);
+pushSteps(stepDown, stepsYellow,4);
+pushSteps(stepRight, stepsYellow,4);
+pushSteps(stepDown, stepsYellow,2);
+pushSteps(stepLeft, stepsYellow,4);
+pushSteps(stepDown, stepsYellow,4);
+pushSteps(stepLeft, stepsYellow,1);
+pushSteps(stepUp, stepsYellow,5);
+
+//Blue token
+pushSteps(stepLeft, stepsBlue,4);
+pushSteps(stepDown, stepsBlue,4);
+pushSteps(stepLeft, stepsBlue,2);
+pushSteps(stepUp, stepsBlue,4,2);
+pushSteps(stepLeft, stepsBlue,4);
+pushSteps(stepUp, stepsBlue,2);
+pushSteps(stepRight, stepsBlue,4);
+pushSteps(stepUp, stepsBlue,4);
+pushSteps(stepRight, stepsBlue,2);
+pushSteps(stepDown, stepsBlue,4);
+pushSteps(stepRight, stepsBlue,4);
+pushSteps(stepDown, stepsBlue,1);
+pushSteps(stepLeft, stepsBlue,5);
+
+//Green token
+pushSteps(stepRight, stepsGreen,4);
+pushSteps(stepUp, stepsGreen,4);
+pushSteps(stepRight, stepsGreen,2);
+pushSteps(stepDown, stepsGreen,4);
+pushSteps(stepRight, stepsGreen,4);
+pushSteps(stepDown, stepsGreen,2);
+pushSteps(stepLeft, stepsGreen,4);
+pushSteps(stepDown, stepsGreen,4);
+pushSteps(stepLeft, stepsGreen,2);
+pushSteps(stepUp, stepsGreen,4);
+pushSteps(stepLeft, stepsGreen,4);
+pushSteps(stepUp, stepsGreen,1);
+pushSteps(stepRight, stepsGreen,5);
+
+function randomMove(Color) {
+    var num = 1;
+    var position = currPos;
+    currcolor = Color;
+    switch (Color) {
+        case "red":
+            for (i = currPos; i < position + num; i++) {
+                stepsRed[i]();
+            }
+            break;
+
+        case "yellow":
+            for (i = currPos; i < position + num; i++) {
+                stepsYellow[i]();
+            }
+            break;
+
+        case "blue":
+            for (i = currPos; i < position + num; i++) {
+                stepsBlue[i]();
+            }
+            break;
+
+        case "green":
+            for (i = currPos; i < position + num; i++) {
+                stepsGreen[i]();
+            }
+            break;
+    }
+    console.log(currPos);
+}
